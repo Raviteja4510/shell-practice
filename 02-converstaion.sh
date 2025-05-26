06a752c9391129bb28098baf6e07592711830e30
vraviteja <vemularavi4510.com>
2025-05-26 19:51:30 +0530

    hello world started

--- 02-converstaion.sh
+++ 02-converstaion.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo "trump:: hey musk,hoe are you?"
+echo "musk:: hello trump,Ima fine.how are you doing"
+echo "trump:: I am fine too. what's up?"
+echo "musk:: Nothing, just going to Mars now, will you come?"
+echo "trump:: sorry,you carry on! I will come once you came back"
