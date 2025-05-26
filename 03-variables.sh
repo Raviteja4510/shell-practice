dd032785b5f9d6d8c124d5d031c4b336b5f24380
vraviteja <vemularavi4510.com>
2025-05-26 20:12:14 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: hey $PERSON2,hoe are you?"
+echo "$PERSON2:: hello $PERSON1,Ima fine.how are you doing"
+echo "$PERSON1:: I am fine too. what's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will you come?"
+echo "$PERSON1:: sorry,you carry on! I will come once you came back"
