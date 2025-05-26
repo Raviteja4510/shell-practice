463fae9bf5cd9d1db849ef979553e0e681d3daf4
vraviteja <vemularavi4510.com>
2025-05-26 20:02:23 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSOM1=
+echo "trump:: hey musk,hoe are you?"
+echo "musk:: hello trump,Ima fine.how are you doing"
+echo "trump:: I am fine too. what's up?"
+echo "musk:: Nothing, just going to Mars now, will you come?"
+echo "trump:: sorry,you carry on! I will come once you came back"
