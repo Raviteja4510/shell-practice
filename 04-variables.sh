e841ec1ab33c12a54bb9ac309bd2f7c7205385a6
vraviteja <vemularavi4510.com>
2025-05-26 20:29:44 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hey $PERSON2,hoe are you?"
+echo "$PERSON2:: hello $PERSON1,Ima fine.how are you doing"
+echo "$PERSON1:: I am fine too. what's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will you come?"
+echo "$PERSON1:: sorry,you carry on! I will come once you came back"
